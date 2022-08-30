e94f0cb072b1830fa69246ab01d4be3ff6def85e
WebBreacher <WebBreacher@users.noreply.github.com>
2022-08-30 13:36:06 -0400

    updating plugins and network grpah

--- .obsidian/plugins/obsidian-smart-typography/main.js
+++ .obsidian/plugins/obsidian-smart-typography/main.js
@@ -8,7 +8,6 @@ if you want to view the source visit the plugins github repository
 var obsidian = require('obsidian');
 var state = require('@codemirror/state');
 var language = require('@codemirror/language');
-var streamParser = require('@codemirror/stream-parser');
 
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
@@ -34,6 +33,263 @@ function __awaiter(thisArg, _arguments, P, generator) {
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 }
+
+// Dashes
+const dashRules = [
+    // en dash
+    {
+        trigger: "-",
+        from: "--",
+        to: "–",
+        contextMatch: /-$/,
+    },
+    // em dash
+    {
+        trigger: "-",
+        from: "–-",
+        to: "—",
+        contextMatch: /–$/,
+    },
+    // tripple dash
+    {
+        trigger: "-",
+        from: "—-",
+        to: "---",
+        contextMatch: /—$/,
+    },
+];
+const dashRulesSansEnDash = [
+    // em dash
+    {
+        trigger: "-",
+        from: "--",
+        to: "—",
+        contextMatch: /-$/,
+    },
+    // tripple dash
+    {
+        trigger: "-",
+        from: "—-",
+        to: "---",
+        contextMatch: /—$/,
+    },
+];
+// Ellipsis
+const ellipsisRules = [
+    {
+        trigger: ".",
+        from: "...",
+        to: "…",
+        contextMatch: /\.\.$/,
+    },
+];
+// Quotes
+const smartQuoteRules = [
+    // Open double
+    {
+        trigger: '"',
+        from: '"',
+        to: (settings) => settings.openDouble,
+        contextMatch: /[\s\{\[\(\<'"\u2018\u201C]$/,
+    },
+    // Close double
+    {
+        trigger: '"',
+        from: '"',
+        to: (settings) => settings.closeDouble,
+        contextMatch: /.*$/,
+    },
+    // Paired double
+    {
+        trigger: '""',
+        from: '""',
+        to: (settings) => settings.openDouble + settings.closeDouble,
+        contextMatch: /.*$/,
+    },
+    // Open single
+    {
+        trigger: "'",
+        from: "'",
+        to: (settings) => settings.openSingle,
+        contextMatch: /[\s\{\[\(\<'"\u2018\u201C]$/,
+    },
+    // Close single
+    {
+        trigger: "'",
+        from: "'",
+        to: (settings) => settings.closeSingle,
+        contextMatch: /.*$/,
+    },
+    // Paired single
+    {
+        trigger: "''",
+        from: "''",
+        to: (settings) => settings.openSingle + settings.closeSingle,
+        contextMatch: /.*$/,
+    },
+];
+// Arrows
+const arrowRules = [
+    {
+        trigger: "-",
+        from: "<-",
+        to: (settings) => settings.leftArrow,
+        contextMatch: /<$/,
+    },
+    {
+        trigger: ">",
+        from: "->",
+        to: (settings) => settings.rightArrow,
+        contextMatch: /-$/,
+    },
+];
+// Guillemet
+const guillemetRules = [
+    {
+        trigger: "<",
+        from: "<<",
+        to: (settings) => settings.openGuillemet,
+        contextMatch: /<$/,
+    },
+    {
+        trigger: ">",
+        from: ">>",
+        to: (settings) => settings.closeGuillemet,
+        contextMatch: />$/,
+    },
+];
+// Comparisons
+const comparisonRules = [
+    {
+        trigger: "=",
+        from: ">=",
+        to: "≥",
+        contextMatch: />$/,
+    },
+    {
+        trigger: "=",
+        from: "<=",
+        to: "≤",
+        contextMatch: /<$/,
+    },
+    {
+        trigger: "=",
+        from: "/=",
+        to: "≠",
+        contextMatch: /\/$/,
+    },
+];
+// Fractions
+const fractionRules = [
+    {
+        trigger: "2",
+        from: "1/2",
+        to: "½",
+        contextMatch: /(?:^|\s)1\/$/,
+    },
+    {
+        trigger: "3",
+        from: "1/3",
+        to: "⅓",
+        contextMatch: /(?:^|\s)1\/$/,
+    },
+    {
+        trigger: "3",
+        from: "2/3",
+        to: "⅔",
+        contextMatch: /(?:^|\s)2\/$/,
+    },
+    {
+        trigger: "4",
+        from: "1/4",
+        to: "¼",
+        contextMatch: /(?:^|\s)1\/$/,
+    },
+    {
+        trigger: "4",
+        from: "3/4",
+        to: "¾",
+        contextMatch: /(?:^|\s)3\/$/,
+    },
+    {
+        trigger: "5",
+        from: "1/5",
+        to: "⅕",
+        contextMatch: /(?:^|\s)1\/$/,
+    },
+    {
+        trigger: "5",
+        from: "2/5",
+        to: "⅖",
+        contextMatch: /(?:^|\s)2\/$/,
+    },
+    {
+        trigger: "5",
+        from: "3/5",
+        to: "⅗",
+        contextMatch: /(?:^|\s)3\/$/,
+    },
+    {
+        trigger: "5",
+        from: "4/5",
+        to: "⅘",
+        contextMatch: /(?:^|\s)4\/$/,
+    },
+    {
+        trigger: "6",
+        from: "1/6",
+        to: "⅙",
+        contextMatch: /(?:^|\s)1\/$/,
+    },
+    {
+        trigger: "6",
+        from: "5/6",
+        to: "⅚",
+        contextMatch: /(?:^|\s)5\/$/,
+    },
+    {
+        trigger: "7",
+        from: "1/7",
+        to: "⅐",
+        contextMatch: /(?:^|\s)1\/$/,
+    },
+    {
+        trigger: "8",
+        from: "1/8",
+        to: "⅛",
+        contextMatch: /(?:^|\s)1\/$/,
+    },
+    {
+        trigger: "8",
+        from: "3/8",
+        to: "⅜",
+        contextMatch: /(?:^|\s)3\/$/,
+    },
+    {
+        trigger: "8",
+        from: "5/8",
+        to: "⅝",
+        contextMatch: /(?:^|\s)5\/$/,
+    },
+    {
+        trigger: "8",
+        from: "7/8",
+        to: "⅞",
+        contextMatch: /(?:^|\s)7\/$/,
+    },
+    {
+        trigger: "9",
+        from: "1/9",
+        to: "⅑",
+        contextMatch: /(?:^|\s)1\/$/,
+    },
+    {
+        trigger: "0",
+        from: "1/10",
+        to: "⅒",
+        contextMatch: /(?:^|\s)1\/1$/,
+    },
+];
 
 const dashChar = "-";
 const enDashChar = "–";
@@ -306,263 +562,6 @@ const legacyComparisonRules = [
 const legacyArrowRules = [leftArrow, rightArrow];
 const legacyGuillemetRules = [leftGuillemet, rightGuillemet];
 
-// Dashes
-const dashRules = [
-    // en dash
-    {
-        trigger: "-",
-        from: "--",
-        to: "–",
-        contextMatch: /-$/,
-    },
-    // em dash
-    {
-        trigger: "-",
-        from: "–-",
-        to: "—",
-        contextMatch: /–$/,
-    },
-    // tripple dash
-    {
-        trigger: "-",
-        from: "—-",
-        to: "---",
-        contextMatch: /—$/,
-    },
-];
-const dashRulesSansEnDash = [
-    // em dash
-    {
-        trigger: "-",
-        from: "--",
-        to: "—",
-        contextMatch: /-$/,
-    },
-    // tripple dash
-    {
-        trigger: "-",
-        from: "—-",
-        to: "---",
-        contextMatch: /—$/,
-    },
-];
-// Ellipsis
-const ellipsisRules = [
-    {
-        trigger: ".",
-        from: "...",
-        to: "…",
-        contextMatch: /\.\.$/,
-    },
-];
-// Quotes
-const smartQuoteRules = [
-    // Open double
-    {
-        trigger: '"',
-        from: '"',
-        to: (settings) => settings.openDouble,
-        contextMatch: /[\s\{\[\(\<'"\u2018\u201C]$/,
-    },
-    // Close double
-    {
-        trigger: '"',
-        from: '"',
-        to: (settings) => settings.closeDouble,
-        contextMatch: /.*$/,
-    },
-    // Paired double
-    {
-        trigger: '""',
-        from: '""',
-        to: (settings) => settings.openDouble + settings.closeDouble,
-        contextMatch: /.*$/,
-    },
-    // Open single
-    {
-        trigger: "'",
-        from: "'",
-        to: (settings) => settings.openSingle,
-        contextMatch: /[\s\{\[\(\<'"\u2018\u201C]$/,
-    },
-    // Close single
-    {
-        trigger: "'",
-        from: "'",
-        to: (settings) => settings.closeSingle,
-        contextMatch: /.*$/,
-    },
-    // Paired single
-    {
-        trigger: "''",
-        from: "''",
-        to: (settings) => settings.openSingle + settings.closeSingle,
-        contextMatch: /.*$/,
-    },
-];
-// Arrows
-const arrowRules = [
-    {
-        trigger: "-",
-        from: "<-",
-        to: (settings) => settings.leftArrow,
-        contextMatch: /<$/,
-    },
-    {
-        trigger: ">",
-        from: "->",
-        to: (settings) => settings.rightArrow,
-        contextMatch: /-$/,
-    },
-];
-// Guillemet
-const guillemetRules = [
-    {
-        trigger: "<",
-        from: "<<",
-        to: (settings) => settings.openGuillemet,
-        contextMatch: /<$/,
-    },
-    {
-        trigger: ">",
-        from: ">>",
-        to: (settings) => settings.closeGuillemet,
-        contextMatch: />$/,
-    },
-];
-// Comparisons
-const comparisonRules = [
-    {
-        trigger: "=",
-        from: ">=",
-        to: "≥",
-        contextMatch: />$/,
-    },
-    {
-        trigger: "=",
-        from: "<=",
-        to: "≤",
-        contextMatch: /<$/,
-    },
-    {
-        trigger: "=",
-        from: "/=",
-        to: "≠",
-        contextMatch: /\/$/,
-    },
-];
-// Fractions
-const fractionRules = [
-    {
-        trigger: "2",
-        from: "1/2",
-        to: "½",
-        contextMatch: /(?:^|\s)1\/$/,
-    },
-    {
-        trigger: "3",
-        from: "1/3",
-        to: "⅓",
-        contextMatch: /(?:^|\s)1\/$/,
-    },
-    {
-        trigger: "3",
-        from: "2/3",
-        to: "⅔",
-        contextMatch: /(?:^|\s)2\/$/,
-    },
-    {
-        trigger: "4",
-        from: "1/4",
-        to: "¼",
-        contextMatch: /(?:^|\s)1\/$/,
-    },
-    {
-        trigger: "4",
-        from: "3/4",
-        to: "¾",
-        contextMatch: /(?:^|\s)3\/$/,
-    },
-    {
-        trigger: "5",
-        from: "1/5",
-        to: "⅕",
-        contextMatch: /(?:^|\s)1\/$/,
-    },
-    {
-        trigger: "5",
-        from: "2/5",
-        to: "⅖",
-        contextMatch: /(?:^|\s)2\/$/,
-    },
-    {
-        trigger: "5",
-        from: "3/5",
-        to: "⅗",
-        contextMatch: /(?:^|\s)3\/$/,
-    },
-    {
-        trigger: "5",
-        from: "4/5",
-        to: "⅘",
-        contextMatch: /(?:^|\s)4\/$/,
-    },
-    {
-        trigger: "6",
-        from: "1/6",
-        to: "⅙",
-        contextMatch: /(?:^|\s)1\/$/,
-    },
-    {
-        trigger: "6",
-        from: "5/6",
-        to: "⅚",
-        contextMatch: /(?:^|\s)5\/$/,
-    },
-    {
-        trigger: "7",
-        from: "1/7",
-        to: "⅐",
-        contextMatch: /(?:^|\s)1\/$/,
-    },
-    {
-        trigger: "8",
-        from: "1/8",
-        to: "⅛",
-        contextMatch: /(?:^|\s)1\/$/,
-    },
-    {
-        trigger: "8",
-        from: "3/8",
-        to: "⅜",
-        contextMatch: /(?:^|\s)3\/$/,
-    },
-    {
-        trigger: "8",
-        from: "5/8",
-        to: "⅝",
-        contextMatch: /(?:^|\s)5\/$/,
-    },
-    {
-        trigger: "8",
-        from: "7/8",
-        to: "⅞",
-        contextMatch: /(?:^|\s)7\/$/,
-    },
-    {
-        trigger: "9",
-        from: "1/9",
-        to: "⅑",
-        contextMatch: /(?:^|\s)1\/$/,
-    },
-    {
-        trigger: "0",
-        from: "1/10",
-        to: "⅒",
-        contextMatch: /(?:^|\s)1\/1$/,
-    },
-];
-
 const DEFAULT_SETTINGS = {
     curlyQuotes: true,
     emDash: true,
@@ -735,7 +734,7 @@ class SmartTypography extends obsidian.Plugin {
                             tree = language.syntaxTree(tr.state);
                         const nodeProps = tree
                             .resolveInner(pos, 1)
-                            .type.prop(streamParser.tokenClassNodeProp);
+                            .type.prop(language.tokenClassNodeProp);
                         if (nodeProps && ignoreListRegEx.test(nodeProps)) {
                             seenPositions[pos] = false;
                         }
